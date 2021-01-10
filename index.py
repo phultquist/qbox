73a0b410bd115f0c02c1e5d2b9e9e158b0c80443
phultquist <phultquist@imsa.edu>
2021-01-10 01:23:19 -0600

    latest

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
-electromagnetic_strength = 1
-damping = -5*1000
-spring_coefficient = -100
+electromagnetic_strength = 10000
+damping = -100
+spring_coefficient = -0.01
 
 samplerate, data = wavfile.read('./test.wav')
 print("Total Data Length:", len(data), ", Sample Rate:",samplerate)
@@ -14,8 +14,10 @@ def integral(current, last):
 
 # data = [0,1,0,1,0,2,1]
 
+data = [e * 0.001 for e in data]
+
 beg = 50000
-end = 50500
+end = 60000
 # beg = 0
 # end = len(data)
 
@@ -25,8 +27,7 @@ last_input_integral_2 = 0
 
 values = []
 while time<end:
-    # electromagnetic_force = [e * electromagnetic_strength for e in data[0:time]] # ElectromagneticStrength * InputSignal
-    electromagnetic_force = electromagnetic_strength * data[time]
+    electromagnetic_force = electromagnetic_strength * data[time] # ElectromagneticStrength * InputSignal
     
     input_integral = integral(data[time], last_input_integral_1) # Integral(InputSignal)
     input_integral_2 = integral(input_integral, last_input_integral_2)  # Integral(Integral(InputSignal))
@@ -38,7 +39,8 @@ while time<end:
     last_input_integral_2 = input_integral_2
 
     output = electromagnetic_force - (damped_signal + springed_signal)
-    print(data[time], output)
+    output = -1*(damped_signal + springed_signal)
+
     values.append(output)
 
     time += 1
@@ -47,9 +49,11 @@ while time<end:
 # plt.show()
 
 fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle('Vertically stacked subplots')
-ax1.plot(data)
+fig.suptitle('Original and Adjusted Waves')
+ax1.plot(data[beg:end])
+ax1.set_title("Original")
 ax2.plot(values)
+ax2.set_title("Adjusted")
 plt.show()
 
 quit()
